78a70e879f49b5132f02b1ac92255622fa7f43b2
alextorres94 <torres.erades.alex@gmail.com>
2019-09-01 19:38:41 +0200

    Update web page

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Personal web page
+# My awesome web page
+🚀
--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 (function cursorAnimation() {
-  let cursor = document.getElementById('cursor');
+  let cursor = document.getElementById('cursor')
   setTimeout(function(){
     if (cursor.style.visibility === "visible") {
-      cursor.style.visibility = "hidden";
+      cursor.style.visibility = "hidden"
     } else {
-        cursor.style.visibility = "visible";
+        cursor.style.visibility = "visible"
     }
     cursorAnimation()
-    }, 500);
-})();
-let awesomeText = document.getElementById("awesomeText");
-let emoji = '';
-let text = 'Hey, I\'m Alex Torres ';
-let num = 0;
+    }, 500)
+})()
+let awesomeText = document.getElementById("awesomeText")
+let emoji = ''
+const text = 'Hey, I\'m Alex Torres '
+let num = 0
 var refreshIntervalId = setInterval(function write(){
-  num += 1;
+  num += 1
   awesomeText.innerHTML = text.slice(0,num) + emoji
   if(num === 21) {
-    emoji = '👋🏼';
-  };
+    emoji = '👋🏼'
+  }
   if(num === 22){
     clearInterval(refreshIntervalId)
   }
